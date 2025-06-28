01f9f8d3726f69938d631713a853ed8a01b20c1a
Swetabh333 <swetabh333@gmail.com>
2025-06-29 00:57:32 +0530

    caesar cipher

---
+++ caesar_cipher/main.rb
@@ -0,0 +1,23 @@
+def caesar_cipher(str,num)
+  unless str.is_a?(String) && num.is_a?(Numeric)
+    puts "Please enter correct arguments"
+    return
+  end
+  res = ""
+  str.each_byte do |char|
+   c = 0
+   if char >= 65 && char <= 90
+    c = char - 65
+    c = (c + num) % 26
+    c += 65
+    elsif char >= 97 && char <= 122
+    c = char - 97
+    c = (c + num) % 26
+    c += 97
+   end
+    res << c.chr 
+  end
+  res
+end
+
+puts caesar_cipher("AeZ",2)
--- nested_collections/hash.rb
+++ nested_collections/hash.rb
@@ -6,7 +6,7 @@ vehicles = {
 
 puts vehicles[:alice][:year] #=> 2019
 
-#puts vehicles[:zoe][:year]
+puts vehicles[:zoe][:year]
 #=> NoMethodError
 
 vehicles.dig(:zoe,:year) #=> nil 
@@ -16,3 +16,32 @@ vehicles.dig(:zoe,:year) #=> nil
 vehicles[:dave] = {:year => 2021 , make: "Ford" , model: "Escape"}
 
 puts vehicles[:dave]
+
+vehicles[:dave][:color] = "red"
+
+#deleting 
+
+vehicles.delete(:blake)
+
+# To delete an element in a nested hash
+#
+
+vehicles[:dave].delete(:color)
+
+puts vehicles.select { |name,data| data[:year] >= 2020 }
+
+# what if you only want the names 
+#
+vehicles.collect { |name,data| name if data[:year] >= 2020 }
+
+#=> [nil, :caleb, :dave]
+
+# To take care of the nil values
+#
+vehicles.collect { |name,data| name if data[:year] >= 2020 }.compact #=> [:caleb, :dave]
+
+vehicles.filter_map { |name, data| name if data[:year] >= 2020 }
+#=> [:caleb, :dave]
+#combines the functionality of the two 
+
+
