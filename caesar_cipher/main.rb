fcd1e594615451c543c3b39db4dd482024af2375
Swetabh333 <swetabh333@gmail.com>
2025-06-29 01:02:13 +0530

    caesar cipher

--- caesar_cipher/main.rb
+++ caesar_cipher/main.rb
@@ -1,23 +1,23 @@
 def caesar_cipher(str,num)
   unless str.is_a?(String) && num.is_a?(Numeric)
-    puts "Please enter correct arguments"
+    puts "Please enter valid arguments"
     return
   end
   res = ""
   str.each_byte do |char|
-   c = 0
-   if char >= 65 && char <= 90
-    c = char - 65
-    c = (c + num) % 26
-    c += 65
+    c = 0
+    if char >= 65 && char <= 90
+      c = char - 65
+      c = (c + num) % 26
+      c += 65
     elsif char >= 97 && char <= 122
-    c = char - 97
-    c = (c + num) % 26
-    c += 97
-   end
-    res << c.chr 
+      c = char - 97
+      c = (c + num) % 26
+      c+= 97
+    end
+    res << c.chr
   end
   res
 end
 
-puts caesar_cipher("AeZ",2)
+puts caesar_cipher("AAbCdef",5)
